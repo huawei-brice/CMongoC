271c33e1d87e566aced9d2f319af68119e92f9be
Brice Dobry <brice.dobry@huawei.com>
2016-08-12 16:46:00 -0400

    Move `pkgConfig` before `dependencies`

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "CMongoC",
+    pkgConfig: "libmongoc-1.0",
     dependencies: [
         .Package(url: "https://github.com/huawei-brice/CBSON.git", majorVersion: 1)
-    ],
-    pkgConfig: "libmongoc-1.0"
+    ]
 )
