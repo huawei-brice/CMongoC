ccbe540e3d343141ea8f3bf34db2a3fdd3d57134
Brice Dobry <brice.dobry@huawei.com>
2016-08-12 12:20:33 -0400

    Use version 1.0 of CBSON

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CMongoC",
     dependencies: [
-        .Package(url: "https://github.com/huawei-brice/CBSON.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/huawei-brice/CBSON.git", majorVersion: 1)
     ],
     pkgConfig: "libmongoc-1.0"
 )
